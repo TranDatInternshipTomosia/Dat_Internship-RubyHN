1f7ffdf41fedbcec04d73161d9e7cdf4d6d2a4f6
TranDatInternshipTomosia <tt.dat.tran@tomosia>
2023-03-16 17:37:47 +0700

    rubu on rails chapter 7

--- learn-ruby/sample_app/app/controllers/users_controller.rb
+++ learn-ruby/sample_app/app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome #{@user.name} to the Sample App"
+      # Handle a successful save.
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+  end
 end
