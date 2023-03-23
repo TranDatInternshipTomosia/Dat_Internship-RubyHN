49a5e184912142c2dd68e6c5275e1a6bca8d8a38
TranDatInternshipTomosia <tt.dat.tran@tomosia>
2023-03-23 17:07:09 +0700

    Ruby on rails

---
+++ Sample_app2/app/models/note.rb
@@ -0,0 +1,7 @@
+class Note < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :description, presence: true
+end
